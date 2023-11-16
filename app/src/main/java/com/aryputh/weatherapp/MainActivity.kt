77d5adbf19e3d59fc09f77baf43c15cd5b87e090
HappyGamer3194 <happygamer3194@gmail.com>
2023-11-15 22:20:54 -0800

    Initial Commit
    Initial Commit has basic UI elements and formatting, no API use yet.

---
+++ app/src/main/java/com/aryputh/weatherapp/MainActivity.kt
@@ -0,0 +1,217 @@
+package com.aryputh.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.aryputh.weatherapp.ui.theme.DarkPurple
+import com.aryputh.weatherapp.ui.theme.Purple40
+import com.aryputh.weatherapp.ui.theme.VeryLightGray
+import com.aryputh.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme (
+                dynamicColor = false
+            ){
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    WeatherPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherPage(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 64.dp, bottom = 64.dp, start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        HeaderImage()
+        MainInfo()
+        ItemTable()
+    }
+}
+
+@Composable
+fun HeaderImage(modifier: Modifier = Modifier)
+{
+    Image(
+        painter = painterResource(id = R.drawable.header_image),
+        contentDescription = null,
+        modifier
+            .width(250.dp)
+            .height(250.dp)
+    )
+}
+
+@Composable
+fun MainInfo(modifier: Modifier = Modifier)
+{
+    Column (
+        modifier = modifier
+            .padding(top = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "11°",
+            color = DarkPurple,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Pullman, WA",
+            color = DarkPurple,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = modifier
+                .padding(top = 16.dp)
+        )
+        Text(
+            text = "Rainy with a chance of rain.\nHigh winds ~10-15 mph.",
+            color = Color.Gray,
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Normal,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .padding(vertical = 24.dp)
+        )
+    }
+}
+
+@Composable
+fun ItemTable(modifier: Modifier = Modifier)
+{
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .background(if (isSystemInDarkTheme()) Color.Black.copy(alpha = 0.2F) else Color.LightGray.copy(alpha = 0.2F))
+    ){
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ){
+            InfoItem(
+                iconRes = R.drawable.humidity,
+                title = "Humidity",
+                subtitle = "85%",
+                modifier = modifier
+            )
+            InfoItem(
+                iconRes = R.drawable.uv_index,
+                title = "UV Index",
+                subtitle = "2 of 10",
+                modifier = modifier
+            )
+        }
+
+        Divider(
+            color = Color.LightGray,
+            modifier = modifier
+                .padding(horizontal = 20.dp)
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ){
+            InfoItem(
+                iconRes = R.drawable.sunrise,
+                title = "Sunrise",
+                subtitle = "7:30 AM",
+                modifier = modifier
+            )
+            InfoItem(
+                iconRes = R.drawable.sunset,
+                title = "Sunset",
+                subtitle = "4:28 PM",
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoItem(@DrawableRes iconRes: Int, title: String, subtitle: String, modifier: Modifier)
+{
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = null,
+            contentScale = ContentScale.Inside,
+            modifier = modifier
+                .padding(end = 8.dp)
+                .width(40.dp)
+        )
+        Column {
+            Text(
+                text = title,
+                color = DarkPurple,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Medium
+            )
+            Text(
+                text = subtitle,
+                color = Color.Gray,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 390, heightDp = 800)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        WeatherPage()
+    }
+}
---
+++ app/src/main/java/com/aryputh/weatherapp/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.aryputh.weatherapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.aryputh.weatherapp.R
+
+val MyCustomFont = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_semibold, FontWeight.Bold)
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = MyCustomFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(fontFamily = MyCustomFont),
+    displayMedium = TextStyle(fontFamily = MyCustomFont),
+    displaySmall = TextStyle(fontFamily = MyCustomFont),
+    headlineLarge = TextStyle(fontFamily = MyCustomFont),
+    headlineMedium = TextStyle(fontFamily = MyCustomFont),
+    headlineSmall = TextStyle(fontFamily = MyCustomFont),
+    titleLarge = TextStyle(fontFamily = MyCustomFont),
+    titleMedium = TextStyle(fontFamily = MyCustomFont),
+    titleSmall = TextStyle(fontFamily = MyCustomFont),
+    bodyMedium = TextStyle(fontFamily = MyCustomFont),
+    bodySmall = TextStyle(fontFamily = MyCustomFont),
+    labelLarge = TextStyle(fontFamily = MyCustomFont),
+    labelMedium = TextStyle(fontFamily = MyCustomFont),
+    labelSmall = TextStyle(fontFamily = MyCustomFont)
+)
